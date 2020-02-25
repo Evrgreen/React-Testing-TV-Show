0654de61ffc9540d036d06c816698a352f509db9
Evrgreen <rcarsten06@gmail.com>
2020-02-25 00:21:07 -0700

    Refacted axios call to fetchShow.js

--- src/App.js
+++ src/App.js
@@ -7,47 +7,53 @@ import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+import fetchShow from "./api/fetchShow";
 
 export default function App() {
   const [show, setShow] = useState(null);
-  const [seasons, setSeasons] = useState(null);
+  const [seasons, setSeasons] = useState([]);
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
+    const fetchByApi = async () => {
+      const response = await fetchShow();
+      setShow(response);
+      setSeasons(formatSeasons(response._embedded.episodes));
     };
-    fetchShow();
+    fetchByApi();
   }, []);
 
-  const handleSelect = e => {
+  const handleSelect = (e) => {
     setSelectedSeason(e.value);
   };
 
   if (!show) {
     return <h2>Fetching data...</h2>;
   }
-
+  console.log(show);
   return (
     <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
-      <h1>{show.name}</h1>
-      {parse(show.summary)}
-      <Dropdown
-        options={Object.keys(seasons)}
-        onChange={handleSelect}
-        value={selectedSeason || "Select a season"}
-        placeholder="Select an option"
-      />
-      <Episodes episodes={episodes} />
+      {!seasons ? (
+        <h2>Fetching data...</h2>
+      ) : (
+        <>
+          <img
+            className="poster-img"
+            src={show.image.original}
+            alt={show.name}
+          />
+          <h1>{show.name}</h1>
+          {parse(show.summary)}
+          <Dropdown
+            options={Object.keys(seasons)}
+            onChange={handleSelect}
+            value={selectedSeason || "Select a season"}
+            placeholder="Select an option"
+          />
+          <Episodes episodes={episodes} />
+        </>
+      )}
     </div>
   );
 }
--- src/utils/formatSeasons.js
+++ src/utils/formatSeasons.js
@@ -1,6 +1,7 @@
-export const formatSeasons = allEpisodes => {
+export const formatSeasons = (allEpisodes) => {
+  console.log("Formating", allEpisodes);
   const seasons = {};
-  allEpisodes.forEach(e => {
+  allEpisodes.forEach((e) => {
     if (!seasons.hasOwnProperty(`Season ${e.season}`)) {
       seasons[`Season ${e.season}`] = [];
     }
